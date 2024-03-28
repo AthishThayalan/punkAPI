5557a61ebd6a7eb7c8a414e90af5afb47969be9f
Athish Thayalan <tathish@hotmail.co.uk>
2024-03-28 09:39:08 +0000

    Created and styled the borders for navbar and main section. Now need to fill contents

---
+++ src/Components/Main/Main.tsx
@@ -0,0 +1,9 @@
+import "./Main.scss";
+const Main = () => {
+  return (
+    <div className="main">
+      <p>Content here...</p>
+    </div>
+  );
+};
+export default Main;
---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import "./Navbar.scss";
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <p>filter here ...</p>
+    </div>
+  );
+};
+export default Navbar;
